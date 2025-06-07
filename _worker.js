3e8e1b28796b732f11d5f2fa86f0834844caf67d
liuhao <hl2659@columbia.edu>
2025-06-07 13:37:54 -0400

    update project to deploy to cloudflare

--- _worker.js
+++ _worker.js
@@ -1,12 +1,12 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    
+
     // Serve index.html for root path
     if (url.pathname === '/') {
       return fetch(new URL('/index.html', request.url));
     }
-    
+
     // Otherwise, serve the requested file
     return fetch(request);
   }
