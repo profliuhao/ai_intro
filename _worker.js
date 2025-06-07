96807ee12d0b872862dadbb919d53ac398355eb2
liuhao <hl2659@columbia.edu>
2025-06-07 13:28:41 -0400

    update project to deploy to cloudflare

---
+++ _worker.js
@@ -0,0 +1,13 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    
+    // Serve index.html for root path
+    if (url.pathname === '/') {
+      return fetch(new URL('/index.html', request.url));
+    }
+    
+    // Otherwise, serve the requested file
+    return fetch(request);
+  }
+};
